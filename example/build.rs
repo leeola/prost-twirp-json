a08eeccb4f647bb59ac2ef40ffba4d5e10c5ffdf
Lee Olayvar <leeola@fastmail.com>
2019-01-10 18:39:51 -0800

    feat: [de]serialize proto structs

--- example/build.rs
+++ example/build.rs
@@ -3,6 +3,7 @@ extern crate prost_yat_build;
 
 fn main() {
     let mut conf = prost_build::Config::new();
+    conf.type_attribute(".", "#[derive(Deserialize, Serialize)]");
     conf.service_generator(Box::new(prost_yat_build::Twirp::new()));
     conf.compile_protos(&["proto/example.proto"], &["proto/"]).unwrap();
 
